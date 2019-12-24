eff0a15f2499aedfa49dd38355478728f5905cde
joy <ss13867776368@163.com>
2019-12-25 06:26:57 +0800

    update attention

--- layers/attention.py
+++ layers/attention.py
@@ -13,18 +13,21 @@ def maskAttentionWeight(w):
     return w * b - 1e9 * (1 - b)
 
 
-def _attention(qkv_bt_3h_r):
+def _attention(n_state, n_head=1):
     """
     qkv_bt_3h_r:
         shape: [B, T, 3H, R]
     """
-    B, T, H_3, R = qkv_bt_3h_r.shape
 
     def init_fun(rng, input_shape):
-        output_shape = (B, T, int(H_3 / 3), R)
+        output_shape = (input_shape[0], input_shape[1], n_head, n_state // n_head)
         return output_shape, ()
-    
+
     def apply_fun(params, inputs, **kwargs):
+        B = inputs.shape[0]
+        T = inputs.shape[1]
+        R = n_state // n_head
+        qkv_bt_3h_r = np.reshape(inputs, [B, T, 3 * n_head, R])
         q_bthr, k_bthr, v_bthr = np.splist(qkv_bt_3h_r, 3, axis=2)
         q_bhtr = np.transpose(q_bthr, [0, 2, 1, 3])
         v_bhtr = np.transpose(v_bthr, [0, 2, 1, 3])
@@ -32,9 +35,33 @@ def _attention(qkv_bt_3h_r):
         W_bhtt = np.matmul(q_bhtr, k_bhrt) / np.sqrt(R)
         W_bhtt = maskAttentionWeight(W_bhtt)
         W_bhtt = stax.softmax(W_bhtt, axis=-1)
-
-        return np.matmul(W_bhtt, v_bhtr)
+        A_bhtr = np.matmul(W_bhtt, v_bhtr)
+        A_bthr = np.transpose(A_bhtr, [0, 2, 1, 3])
+        A_bts = np.reshape(A_bthr, [B, T, n_state])
+        return A_bts
 
     return init_fun, apply_fun
 
-def attention()
+
+def attention(n_state, n_head=1, residual=True):
+    Main = stax.serial(stax.Dense(3 * n_state),
+                       _attention(n_state, n_head),
+                       stax.Dense(n_state))
+    Shortcut = stax.Identity()
+    return stax.serial(stax.FanOut(2), stax.Parallel(Main, Shortcut), stax.FanInSum())
+
+
+def encoderBlock(n_state, n_head=1):
+    Main = stax.serial(attention(n_state, n_head),
+                       stax.BatchNorm(axis=-1),
+                       stax.Dense(4 * n_state),
+                       stax.Relu(),
+                       stax.Dense(n_state))
+    Shortcut = stax.Identity()
+    return stax.serial(stax.FanOut(2), stax.Parallel(Main, Shortcut),
+                       stax.FanInSum(), stax.BatchNorm(axis=-1))
+
+def encoderGroup(n_group, n_state, n_head=1):
+    blocks = []
+    for _ in range(n_group):
+        blocks += [encoderBlock(n_state, n_head)]
